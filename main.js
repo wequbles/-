6d195c4946cde5daa4afc3d80af5187cc237f27d
wequbles <120826725+wequbles@users.noreply.github.com>
2024-01-27 20:46:30 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,38 +30,7 @@ function scrollTo(targetId) {
 	});
 }
 
-/* Кнопки слайдера */
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const scroller = document.getElementById('gallery__scroller');
-
-const scrollerBlocks = document.querySelectorAll('.gallery__block');
-const totalWidthBlocks = Array.from(scrollerBlocks).reduce((i, block) => i + block.offsetWidth, 0);
-const numberOfBlocks = scrollerBlocks.length;
-
-function checkSliderPosition() {
-	const isAtBeginning = scroller.scrollLeft === 0;
-	const isAtEnd = scroller.scrollLeft + scroller.clientWidth === scroller.scrollWidth;
-
-	console.log(scroller.scrollLeft + scroller.clientWidth, scroller.scrollWidth)
-
-	prevBtn.style.display = isAtBeginning ? 'none' : 'block';
-	nextBtn.style.display = isAtEnd ? 'none' : 'block';
-}
-
-scroller.addEventListener('scroll', checkSliderPosition);
-
-nextBtn.addEventListener('click', () => {
-	scroller.scrollBy({ left: (totalWidthBlocks / numberOfBlocks) });
-	checkSliderPosition();
-});
-
-prevBtn.addEventListener('click', () => {
-	scroller.scrollBy({ left: - (totalWidthBlocks / numberOfBlocks) });
-	checkSliderPosition();
-});
-
-checkSliderPosition();
+/*  */
 
 /* Копирование номера телефона */
 const phoneNumberBlock = document.getElementById('phone_number');
@@ -69,9 +38,6 @@ const phoneNumber = phoneNumberBlock.getAttribute('data-phone-number');
 
 phoneNumberBlock.addEventListener('click', function() {
 	navigator.clipboard.writeText(phoneNumber) // Копируем номер телефона в буфер обмена
-		.then(() => {
-			alert('Номер телефона скопирован'); // При необходимости выводим сообщение об успешном копировании
-		})
 		.catch(error => {
 			console.error('Ошибка копирования ' + error);
 		});
@@ -79,5 +45,7 @@ phoneNumberBlock.addEventListener('click', function() {
 	const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent); // Определяем платформу устройства
 	if (isMobileDevice) { // Если устройство мобильное, открываем приложение мобильного телефона
 		window.location.href = `tel:${phoneNumber}`;
+	} else {
+		alert('Номер телефона скопирован');
 	}
 });
