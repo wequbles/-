b5579a0738d6e67e5dfcbd80cde939b25868dfd8
wequbles <120826725+wequbles@users.noreply.github.com>
2024-03-05 17:19:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 "use strict";
 
-const lightBtn = document.getElementById('light');
-lightBtn.addEventListener('click', () => {
-	if (lightBtn.getAttribute('data-light') == 'true') {
-		lightBtn.setAttribute('data-light', 'false');
-	} else {
-		lightBtn.setAttribute('data-light', 'true');
-	}
-});
-
 /* Прокручивание к контактам */
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	anchor.addEventListener('click', function (e) {
@@ -21,7 +12,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 		e.preventDefault();
 		const targetId = this.getAttribute('href');
 		scrollTo(targetId);
-	});
+	},
+	{passive: true});
 });
 
 function scrollTo(targetId) {
@@ -30,7 +22,96 @@ function scrollTo(targetId) {
 	});
 }
 
-/*  */
+/* ripple эффект для кнопки */
+function rippleEffect(event) {
+	const button = event.currentTarget;
+	const ripple = document.createElement('span');
+	const rect = button.getBoundingClientRect();
+	const size = Math.max(rect.width, rect.height);
+	const x = event.clientX - rect.left - size / 2;
+	const y = event.clientY - rect.top - size / 2;
+
+	ripple.style.width = ripple.style.height = `${size}px`;
+	ripple.style.left = `${x}px`;
+	ripple.style.top = `${y}px`;
+
+	ripple.classList.add('ripple');
+
+	button.appendChild(ripple);
+
+		ripple.addEventListener('animationend', () => {
+			ripple.remove();
+		});
+};
+
+const fixedButton = document.getElementById('fixed__btn');
+setInterval(() => {
+	rippleEffect({
+		currentTarget: fixedButton,
+		clientX: fixedButton.getBoundingClientRect().left + fixedButton.offsetWidth / 2,
+		clientY: fixedButton.getBoundingClientRect().top + fixedButton.offsetHeight / 2,
+	});
+}, 4000);
+
+
+document.querySelectorAll('.ripple_btn').forEach(button => {
+	button.addEventListener('click', rippleEffect);
+});
+
+/* Переключатель кнопки связи */
+const activeCont = document.getElementById('fixed');
+const activeBtn = document.getElementById('fixed__btn');
+activeBtn.addEventListener('click', () => {
+	if (activeCont.getAttribute('data-active') == 'true') {
+		activeCont.setAttribute('data-active', 'false');
+	} else {
+		activeCont.setAttribute('data-active', 'true');
+	}
+});
+document.addEventListener('mouseover', (event) => {
+	if (!activeCont.contains(event.target)) {
+		activeCont.setAttribute('data-active', 'false');
+	}
+});
+
+document.addEventListener('mouseout', (event) => {
+	if (!activeCont.contains(event.target)) {
+		activeCont.setAttribute('data-active', 'false');
+	}
+});
+
+/* Кнопка "Показать еще" */
+const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent); // Определяем платформу устройства
+document.addEventListener('DOMContentLoaded', function() {
+	const portfolioElement = document.getElementById('portfolio');
+	const gridItems = portfolioElement.children;
+	const showMoreButton = document.getElementById('showMoreButton');
+	let itemsPerClick;
+	if (isMobileDevice) {
+		itemsPerClick = 3;
+	} else {
+		itemsPerClick = 8;
+	}
+	let visibleItems = itemsPerClick;
+
+	for (let i = itemsPerClick; i < gridItems.length; i++) { // Скрыть все элементы, кроме первых itemsPerClick
+		gridItems[i].style.display = 'none';
+	}
+
+	showMoreButton.addEventListener('click', function() {
+		for (let i = visibleItems; i < visibleItems + itemsPerClick && i < gridItems.length; i++) {
+			gridItems[i].style.display = 'block';
+		}
+		visibleItems += itemsPerClick;
+		if (visibleItems >= gridItems.length) {
+			showMoreButton.style.display = 'none';
+		}
+	});
+
+	if (visibleItems >= gridItems.length) {
+		showMoreButton.style.display = 'none'; // Скрыть кнопку, если больше нет элементов для отображения
+	}
+});
 
 /* Копирование номера телефона */
 const phoneNumberBlock = document.getElementById('phone_number');
@@ -42,7 +123,6 @@ phoneNumberBlock.addEventListener('click', function() {
 			console.error('Ошибка копирования ' + error);
 		});
 
-	const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent); // Определяем платформу устройства
 	if (isMobileDevice) { // Если устройство мобильное, открываем приложение мобильного телефона
 		window.location.href = `tel:${phoneNumber}`;
 	} else {
